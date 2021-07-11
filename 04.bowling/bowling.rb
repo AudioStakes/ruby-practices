29ef505f4ee080537e7ceaceb6a93820a08b14de
AudioStakes <daisuke.sato@enishi-tech.com>
2021-07-11 09:35:07 +0900

    ストライクとスペアの判定をメソッドに切り出す

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,6 +1,14 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+def strike?(frame)
+  frame[0] == 10
+end
+
+def spare?(frame)
+  frame[0] != 10 && frame.sum == 10
+end
+
 # 数値に変換
 scores = ARGV[0].gsub('X', '10').split(',').map(&:to_i)
 
@@ -11,7 +19,7 @@ scores.each do |score|
   if frames.size < 9 # 9フレーム目まで
     frame << score
 
-    if frame[0] == 10 || frame.size == 2 # ストライクもしくは2投目
+    if strike?(frame) || frame.size == 2 # ストライクもしくは2投目
       frames << frame.dup
       frame.clear
     end
@@ -28,10 +36,10 @@ frames.each_with_index do |current_frame, i|
 
   # ストライク・スペアによる得点の加算
   if i < 9 # 9フレーム目まで
-    if current_frame[0] == 10 # ストライク
+    if strike?(current_frame)
       point += frames[i + 1][0]
       point += frames[i + 1][1] || frames[i + 2][0]
-    elsif current_frame.sum == 10 # スペア
+    elsif spare?(current_frame)
       point += frames[i + 1][0]
     end
   end

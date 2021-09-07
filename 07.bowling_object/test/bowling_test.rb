8f9236a1fd85d940f3eea6dc0c24bdaba94748e7
AudioStakes <daisuke.sato@enishi-tech.com>
2021-09-07 12:09:48 +0900

    テストのメソッド名がテストの内容や目的を表すようにした

--- 07.bowling_object/test/bowling_test.rb
+++ 07.bowling_object/test/bowling_test.rb
@@ -4,31 +4,31 @@ require 'minitest/autorun'
 require_relative '../lib/game'
 
 class BowlingTest < Minitest::Test
-  def test_game1
+  def test_spare_in_final_frame
     input = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5'
 
     assert_equal 139, Game.new(input).score
   end
 
-  def test_game2
-    input = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X'
-
-    assert_equal 164, Game.new(input).score
-  end
-
-  def test_game3
-    input = '0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4'
-
-    assert_equal 107, Game.new(input).score
-  end
-
-  def test_game4
+  def test_strike_in_final_frame
     input = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0'
 
     assert_equal 134, Game.new(input).score
   end
 
-  def test_game5
+  def test_turkey_in_final_frame
+    input = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X'
+
+    assert_equal 164, Game.new(input).score
+  end
+
+  def test_turkey_out_of_final_frame
+    input = '0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4'
+
+    assert_equal 107, Game.new(input).score
+  end
+
+  def test_perfect_game
     input = 'X,X,X,X,X,X,X,X,X,X,X,X'
 
     assert_equal 300, Game.new(input).score
